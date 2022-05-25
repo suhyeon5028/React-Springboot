075a2461d5b07b44f976824d58e44790bfbb07c1
suhyeon <suhyeon5028@naver.com>
2022-05-25 15:52:08 +0900

    React-Springboot 서버 만들기 1

---
+++ src/main/java/site/metacoding/book/service/BookService.java
@@ -0,0 +1,11 @@
+package site.metacoding.book.service;
+
+import org.springframework.stereotype.Service;
+
+// 기능을 정의할 수 있고, 트랜잭션을 관리할 수 있음.
+
+@Service
+public class BookService {
+    
+    // 함수 => 송금() -> 레파지토리에 여러개의 함수 실행 -> commit or roolback
+}
